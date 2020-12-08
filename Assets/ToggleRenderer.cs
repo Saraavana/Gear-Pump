5f8fc529c7a2edce75628059f761c0ac8a0da9fe
jh.admin <jh.admin@CL084.SBG.intern>
2020-12-08 14:14:28 +0100

    Initial commit

---
+++ Assets/ToggleRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRenderer : MonoBehaviour
+{
+
+    bool objectVisibility;
+    void Start()
+    {
+        objectVisibility = false;
+        gameObject.SetActive(objectVisibility);
+    }
+
+    public void ToggleVisibility()
+    {
+
+        if (objectVisibility == false)
+            gameObject.SetActive(true);
+        
+        if (objectVisibility==false) 
+        {
+            gameObject.SetActive(true);
+            objectVisibility = true;
+        }    
+        else
+        {
+            gameObject.SetActive(false);
+            objectVisibility = false;
+        }
+    }
+
+    public void showView()
+    {
+        gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/rotateobject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateobject : MonoBehaviour
+{
+    [Tooltip("Speed of rotation of the object in z - axis")]
+    [SerializeField]
+    private float z_speed = 0f;
+    /// <summary>
+    /// Speed of rotation.
+    /// </summary>
+    public float Speed
+    {
+        get
+        {
+            return z_speed;
+        }
+        set
+        {
+            z_speed = value;
+        }
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, z_speed) * Time.deltaTime);
+    }
+}
